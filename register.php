c186efc25467eb881ee6e8a3e62ee65071f98ade
Randy <randya.cramer@gmail.com>
2015-01-19 00:06:34 -0500

    Create register.php

---
+++ register.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <?php echo $messsage; ?>
+    <body>
+        <form action="" method="post">
+            <input type="text" name="name" placeholder="Username">
+            <input type="text" name="email" placeholder="email">
+            <input type="password" name="password" placeholder="password">
+            <input type="submit" value="register">
+        </form>
+    </body>
+</html>
